0dd8920791327cda4009e8a2de38650cbce06ddb
yhujae <yhujayp757@gmail.com>
2024-01-18 00:03:37 +0100

    created portfolio using tainwind

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className=' py-8 ' id='home'>
+      <div className='container mx-auto'>
+        <div className='flex justify-between items-center'>
+          <a href='#'>
+            <span className='mb-0'>PRECIOUS</span>
+            <hr className='my-0 bg-warning h-0.5 w-[90px]' />
+            <span className='my-0 text-secondary'>ROBERTS</span>
+          </a>
+          <button className='btn btn-sm'>Work with me </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
